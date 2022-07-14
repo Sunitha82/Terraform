64af446bbda3b3d40638153fea92d4a2c286d9bf
Sunitha82 <56600171+Sunitha82@users.noreply.github.com>
2022-07-14 14:22:28 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
    region  = "us-east-1"
 }
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "example-tf-bucket"
+resource "aws_s3_bucket" "examplebucket" {
+  bucket = "sunikans"
 }
