128c4fb987102afd84aaff8eff75407dabe1a315
Sunitha82 <56600171+Sunitha82@users.noreply.github.com>
2022-07-14 13:48:29 +0100

    adad
    adfs

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider  "aws" {
   region = "us-east-1"
-  //access_key = "AKIAZ7GWPZ7XJQ27UTIV"
-  //secret_key = "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"
   profile = "default"
 }
 resource "aws_s3_bucket" "abc" {
